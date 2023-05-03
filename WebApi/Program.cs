b8e467489ab00b4cd915acfb14d881360cd06a13
aledo <aledo@DESKTOP-81I1KRD>
2023-05-03 15:20:22 +0200

    JWT in program.cs

--- WebApi/Program.cs
+++ WebApi/Program.cs
@@ -1,4 +1,7 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 using WebApi.Contexts;
 using WebApi.Models.Identity;
 using WebApi.Repositories;
@@ -25,6 +28,39 @@ builder.Services.AddDefaultIdentity<CustomIdentityUser>(x =>
 	x.SignIn.RequireConfirmedAccount = false;
 	x.Password.RequiredLength = 8;
 }).AddEntityFrameworkStores<IdentityContext>();
+
+// JWT
+builder.Services.AddAuthentication(x =>
+{
+	x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+	x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(x =>
+{
+	x.Events = new JwtBearerEvents
+	{
+		OnTokenValidated = context =>
+		{
+			if (string.IsNullOrEmpty(context?.Principal?.FindFirst("id")?.Value) || string.IsNullOrEmpty(context?.Principal?.Identity?.Name))
+				context?.Fail("Unauthorized");
+
+			return Task.CompletedTask;
+		}
+	};
+
+	x.RequireHttpsMetadata = true;
+	x.SaveToken = true;
+	x.TokenValidationParameters = new TokenValidationParameters
+	{
+		ValidateIssuer = true,
+		ValidIssuer = builder.Configuration.GetSection("TokenValidation").GetValue<string>("Issuer")!,
+		ValidateAudience = true,
+		ValidAudience = builder.Configuration.GetSection("TokenValidation").GetValue<string>("Audience")!,
+		ValidateLifetime = true,
+		ValidateIssuerSigningKey = true,
+		IssuerSigningKey = new SymmetricSecurityKey(
+			Encoding.UTF8.GetBytes(builder.Configuration.GetSection("TokenValidation").GetValue<string>("TokenKey")!))
+	};
+});
 
 var app = builder.Build();
 app.UseCors(x => x.AllowAnyHeader().AllowAnyOrigin().AllowAnyMethod());
