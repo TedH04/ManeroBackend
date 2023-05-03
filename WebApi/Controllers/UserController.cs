267a44f3cad7bd459b2e765f72d759bb03329c61
Robin Andersson <robin.andersson@me.com>
2023-05-03 14:16:03 +0200

    Add controller and service

---
+++ WebApi/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.Dtos;
+using WebApi.Services;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(SignUpRequest request)
+        {
+            if (ModelState.IsValid)
+            {
+                if (await _userService.SignUpAsync(request))
+                {
+                    return Created("", );
+                }
+            }
+        }
+    }
+}
---
+++ WebApi/services/UserService.cs
@@ -0,0 +1,50 @@
+﻿using System.Security.Claims;
+using WebApi.Models.Dtos;
+using WebApi.Models.Identity;
+
+namespace WebApi.Services
+{
+    public class UserService
+    {
+        private readonly UserRepository _userRepo;
+
+        public UserService(UserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        public async Task<bool> SignUpAsync(SignUpRequest request)
+        {
+            CustomIdentityUser user = request;
+
+            var createResult = await _userRepo.SignUpAsync(user, request.Password);
+
+            if (createResult.Succeeded)
+            {
+                var signInResult = await _userRepo.SignInAsync(user.Email, request.Password, false, false);
+
+                if (signInResult.Succeeded)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public async Task<string> SignInAsync(SignInRequest request)
+        {
+            var identityUser = await _userRepo.FindUserAsync(request.Email);
+
+            if (identityUser != null)
+            {
+                var signInResult = await _userRepo.SignInAsync(request.Email, request.Password, false, false);
+
+                if (signInResult.Succeeded)
+                {
+                    var claimsIdentity = new ClaimsIdentity
+                }
+            }
+        }
+    }
+}
