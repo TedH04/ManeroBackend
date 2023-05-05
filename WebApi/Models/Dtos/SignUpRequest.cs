bf01dbad8d288ed640bb7c8e143df42e3c5fb0d2
Robin Andersson <robin.andersson@me.com>
2023-05-05 13:35:56 +0200

    Update required error message and add regex to phonenumber

--- WebApi/Models/Dtos/SignUpRequest.cs
+++ WebApi/Models/Dtos/SignUpRequest.cs
@@ -5,31 +5,30 @@ namespace WebApi.Models.Dtos
 {
 	public class SignUpRequest
 	{
-        [Required]
+        [Required(ErrorMessage = "First name is required")]
         [MinLength(2, ErrorMessage = "First name must be atleast 2 characters")]
         public string FirstName { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage = "Last name is required")]
         [MinLength(2, ErrorMessage = "Last name must be atleast 2 characters")]
         public string LastName { get; set; } = null!;
 
-        [Required]
-        [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "Email is required")]
         [MinLength(6, ErrorMessage = "Email must be atleast 6 characters")]
         [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email is in wrong format")]
         public string Email { get; set; } = null!;
 
-        [Required]
-        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "Password is required")]
         [MinLength(8, ErrorMessage = "Password must be atleast 8 characters")]
         [RegularExpression(@"^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[^a-zA-Z0-9]).{8,}$",
             ErrorMessage = "Password must contain at least one uppercase letter, one lowercase letter, one digit, and one special character. It must be at least 8 characters long.")]
         public string Password { get; set; } = null!;
 
-        [Required]
+        [Required(ErrorMessage = "Phonenumber is required")]
+        [RegularExpression(@"^\+(?=[0-9]{2,3})([0-9]{2,3})[0-9]{6,14}$", ErrorMessage = "Phonenumber is in wrong format")]
         public string PhoneNumber { get; set; } = null!;
         
-		    public string? ProfileImage { get; set; }
+		public string? ProfileImage { get; set; }
 
         public static implicit operator CustomIdentityUser(SignUpRequest request)
         {
