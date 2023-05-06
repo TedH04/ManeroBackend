a08b0dd225e7f2303420d0995525b8fc8f5d0eb3
Robin Andersson <robin.andersson@me.com>
2023-05-06 20:47:05 +0200

    Add annotations

--- WebApi/Models/Dtos/SignInRequest.cs
+++ WebApi/Models/Dtos/SignInRequest.cs
@@ -1,8 +1,13 @@
-﻿namespace WebApi.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Dtos
 {
     public class SignInRequest
     {
+        [Required]
         public string Email { get; set; } = null!;
+
+        [Required]
         public string Password { get; set; } = null!;
     }
 }
--- WebApi/Models/Dtos/UserUpdateRequest.cs
+++ WebApi/Models/Dtos/UserUpdateRequest.cs
@@ -1,12 +1,25 @@
-﻿namespace WebApi.Models.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApi.Models.Dtos
 {
     public class UserUpdateRequest
     {
+        [Required]
         public string UserId { get; set; } = null!;
+
+        [MinLength(2, ErrorMessage = "First name must be atleast 2 characters")]
         public string? FirstName { get; set; }
+
+        [MinLength(2, ErrorMessage = "Last name must be atleast 2 characters")]
         public string? LastName { get; set; }
+
+        [MinLength(6, ErrorMessage = "Email must be atleast 6 characters")]
+        [RegularExpression(@"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email is in wrong format")]
         public string? Email { get; set; }
+
+        [RegularExpression(@"^\+(?=[0-9]{2,3})([0-9]{2,3})[0-9]{6,14}$", ErrorMessage = "Phonenumber is in wrong format")]
         public string? PhoneNumber { get; set; }
+
         public string? ProfileImage { get; set; }
     }
 }
